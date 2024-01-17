32c279a4616bc789bbaa3c2a6c1f181c2e1ce1e1
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2024-01-17 10:55:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freqobj={};
+	for(let char in str){
+		freqobj[char]=1;
+		return  char;
+	
+	}
+		return null;
+	}
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
